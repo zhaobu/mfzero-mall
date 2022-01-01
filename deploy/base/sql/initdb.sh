6f40f6c7b6b79a2bf2c589cc6e54bb3f46a6b2d4
lwxmwsl <zhaobulw@qq.com>
2022-01-02 03:48:16 +0800

    增加docker-compose

--- deploy/base/sql/initdb.sh
+++ deploy/base/sql/initdb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dir=$(ls /data/sql/admin)
+dir=$(ls /data/sql)
 # read -p "Enter your username, please: " username
 
 # read -p "Enter your password, please: " pswd
@@ -13,7 +13,7 @@ mysql -h $host -u$user -p$password --default-character-set=utf8mb4 < create_db.s
 
 echo "use mf_admin;" >all.sql
 for i in $dir; do
-    echo "source /data/sql/admin/$i;" >>all.sql
+    echo "source /data/sql/$i;" >>all.sql
 done
 
 mysql -h $host -u $user -p$password --default-character-set=utf8mb4 <all.sql
